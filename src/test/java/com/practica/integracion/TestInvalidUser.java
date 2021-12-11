97fa07e0325050e6a3ec968613b4abf7ad4da19a
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2021-12-11 16:13:15 +0100

    Added tests for StartRemoteSystem, StopRemoteSystem and AddRemoteSystem.

--- src/test/java/com/practica/integracion/TestInvalidUser.java
+++ src/test/java/com/practica/integracion/TestInvalidUser.java
@@ -1,11 +1,111 @@
 package com.practica.integracion;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.practica.integracion.DAO.*;
+import com.practica.integracion.manager.SystemManager;
+import com.practica.integracion.manager.SystemManagerException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.naming.OperationNotSupportedException;
+
 @ExtendWith(MockitoExtension.class)
 public class TestInvalidUser {
-	/**
-	 * RELLENAR POR EL ALUMNO
-	 */
+	GenericDAO mockGenericDao;
+	AuthDAO mockAuthDao;
+	
+	@BeforeEach
+	void setUp() {
+		mockGenericDao = mock(GenericDAO.class);
+		mockAuthDao = mock(AuthDAO.class);
+	}
+	
+	@Test
+	public void TC13() throws Exception {
+		String userId = "aaa";
+		User inValidUser = new User(userId,"Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+		String validId ="12345";
+		when(mockAuthDao.getAuthData(inValidUser.getId())).thenReturn(inValidUser);
+
+		String remoteId = "12345";
+		ArrayList<Object> lista = new ArrayList<>(Arrays.asList("uno", "dos"));
+		when(mockGenericDao.getSomeData(inValidUser, "where id=" + validId)).thenThrow(OperationNotSupportedException.class);
+	  
+		InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
+	  
+		SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
+
+		
+		try {
+			manager.startRemoteSystem(userId, remoteId);
+			fail("Exception not thrown when expected! ");
+			
+		} catch (SystemManagerException e) {
+			ordered.verify(mockAuthDao).getAuthData(inValidUser.getId());
+			ordered.verify(mockGenericDao).getSomeData(inValidUser, "where id=" + remoteId);
+
+		}
+}
+	
+	@Test
+	public void TC23() throws Exception {
+		String userId = "aaa";
+		User inValidUser = new User(userId,"Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+		String validId ="12345";
+		when(mockAuthDao.getAuthData(inValidUser.getId())).thenReturn(inValidUser);
+
+		String remoteId = "12345";
+		ArrayList<Object> lista = new ArrayList<>(Arrays.asList("uno", "dos"));
+		when(mockGenericDao.getSomeData(inValidUser, "where id=" + validId)).thenThrow(OperationNotSupportedException.class);
+	  
+		InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
+	  
+		SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
+
+		
+		try {
+			manager.stopRemoteSystem(userId, remoteId);
+			fail("Exception not thrown when expected! ");
+			
+		} catch (SystemManagerException e) {
+			ordered.verify(mockAuthDao).getAuthData(inValidUser.getId());
+			ordered.verify(mockGenericDao).getSomeData(inValidUser, "where id=" + remoteId);
+
+		}
+	}
+	
+	@Test
+	void TC33() throws OperationNotSupportedException {
+		String userId = "12345";
+		
+		User validUser = new User(userId,"Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+		when(mockAuthDao.getAuthData(validUser.getId())).thenReturn(validUser);
+		
+		String remoteId = "aaa";
+		when(mockGenericDao.updateSomeData(validUser, remoteId)).thenReturn(false);
+		
+		InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
+		SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
+		try {
+			manager.addRemoteSystem(userId, remoteId);
+			fail("Exception not thrown when expected! ");
+		} catch (SystemManagerException e) {
+			
+		ordered.verify(mockAuthDao).getAuthData(validUser.getId());
+	    ordered.verify(mockGenericDao).updateSomeData(validUser, remoteId);
+
+		}
+	}
 }
--- src/test/java/com/practica/integracion/TestValidUser.java
+++ src/test/java/com/practica/integracion/TestValidUser.java
@@ -1,13 +1,194 @@
 package com.practica.integracion;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.practica.integracion.DAO.*;
+import com.practica.integracion.manager.SystemManager;
+import com.practica.integracion.manager.SystemManagerException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.naming.OperationNotSupportedException;
+
+
 @ExtendWith(MockitoExtension.class)
 public class TestValidUser {
+	
+	GenericDAO mockGenericDao;
+	AuthDAO mockAuthDao;
+	
+	@BeforeEach
+	void setUp() {
+		mockGenericDao = mock(GenericDAO.class);
+		mockAuthDao = mock(AuthDAO.class);
+	}
+	
+	@Test
+	public void TC11() throws Exception {
+	  User validUser = new User("1","Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+	  
+	  when(mockAuthDao.getAuthData(validUser.getId())).thenReturn(validUser);
 
-	/**
-	 * RELLENAR POR EL ALUMNO
-	 */
+	  String validId = "12345";
+	  
+	  ArrayList<Object> lista = new ArrayList<>(Arrays.asList("uno", "dos"));
+	  
+	  when(mockGenericDao.getSomeData(validUser, "where id=" + validId)).thenReturn(lista);
+	  
+	  InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
+	  
+	  SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
+	  
+	  Collection<Object> retorno = manager.startRemoteSystem(validUser.getId(), validId);
+	  
+	  assertEquals(retorno.toString(), "[uno, dos]");
+	  
+	  ordered.verify(mockAuthDao).getAuthData(validUser.getId());
+	  ordered.verify(mockGenericDao).getSomeData(validUser, "where id=" + validId);
+	}
+	
+	
+	@Test
+	public void TC12() throws Exception {
+		String userId = "12345";
+		User validUser = new User(userId,"Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+		when(mockAuthDao.getAuthData(validUser.getId())).thenReturn(validUser);
+
+		String remoteId = "aaa";
+		ArrayList<Object> lista = new ArrayList<>(Arrays.asList("uno", "dos"));
+		when(mockGenericDao.getSomeData(validUser, "where id=" + remoteId)).thenThrow(OperationNotSupportedException.class);
+	  
+		InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
+	  
+		SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
+
+		
+		try {
+			manager.startRemoteSystem(userId, remoteId);
+			fail("Exception not thrown when expected! ");
+			
+		} catch (SystemManagerException e) {
+			ordered.verify(mockAuthDao).getAuthData(validUser.getId());
+			ordered.verify(mockGenericDao).getSomeData(validUser, "where id=" + remoteId);
+
+		}
+	  
+	}
+	
+	
+	@Test
+	public void TC21() throws Exception {
+	  User validUser = new User("1","Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+	  
+	  when(mockAuthDao.getAuthData(validUser.getId())).thenReturn(validUser);
+
+	  String validId = "12345";
+	  
+	  ArrayList<Object> lista = new ArrayList<>(Arrays.asList("uno", "dos"));
+	  
+	  when(mockGenericDao.getSomeData(validUser, "where id=" + validId)).thenReturn(lista);
+	  
+	  InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
+	  
+	  SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
+	  
+	  Collection<Object> retorno = manager.stopRemoteSystem(validUser.getId(), validId);
+	  
+	  assertEquals(retorno.toString(), "[uno, dos]");
+	  
+	  ordered.verify(mockAuthDao).getAuthData(validUser.getId());
+	  ordered.verify(mockGenericDao).getSomeData(validUser, "where id=" + validId);
+	}
+	
+	@Test
+	public void TC22() throws Exception {
+		String userId = "12345";
+		User validUser = new User(userId,"Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+		when(mockAuthDao.getAuthData(validUser.getId())).thenReturn(validUser);
+
+		String remoteId = "aaa";
+		ArrayList<Object> lista = new ArrayList<>(Arrays.asList("uno", "dos"));
+		when(mockGenericDao.getSomeData(validUser, "where id=" + remoteId)).thenThrow(OperationNotSupportedException.class);
+	  
+		InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
+	  
+		SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
+
+		
+		try {
+			manager.stopRemoteSystem(userId, remoteId);
+			fail("Exception not thrown when expected! ");
+			
+		} catch (SystemManagerException e) {
+			ordered.verify(mockAuthDao).getAuthData(validUser.getId());
+			ordered.verify(mockGenericDao).getSomeData(validUser, "where id=" + remoteId);
+
+		}
+	  
+	}
+	
+	
+	
+    @Test
+	void TC31() throws OperationNotSupportedException {
+		String userId = "12345";
+		
+		User validUser = new User(userId,"Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+		when(mockAuthDao.getAuthData(validUser.getId())).thenReturn(validUser);
+		
+		String remoteId = "12345";
+		when(mockGenericDao.updateSomeData(validUser, remoteId)).thenReturn(true);
+		
+		InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
+		
+		  
+		SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
+		
+		try {
+			manager.addRemoteSystem(userId, remoteId);
+			
+			ordered.verify(mockAuthDao).getAuthData(validUser.getId());
+			ordered.verify(mockGenericDao).updateSomeData(validUser, remoteId);
+			
+		} catch (SystemManagerException e) {
+			fail("Exception Thrown when not expected! "  + e);
+		}
+	}
+	
+	@Test
+	void TC32() throws OperationNotSupportedException {
+		String userId = "12345";
+		
+		User validUser = new User(userId,"Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+		when(mockAuthDao.getAuthData(validUser.getId())).thenReturn(validUser);
+		
+		String remoteId = "aaa";
+		when(mockGenericDao.updateSomeData(validUser, remoteId)).thenThrow(OperationNotSupportedException.class);
+		
+		InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
+		SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
+		try {
+			manager.addRemoteSystem(userId, remoteId);
+			fail("Exception not thrown when expected! ");
+			
+		} catch (SystemManagerException e) {
+			ordered.verify(mockAuthDao).getAuthData(validUser.getId());
+			ordered.verify(mockGenericDao).updateSomeData(validUser, remoteId);
+
+		}
+	}
+	
+
 
 }
